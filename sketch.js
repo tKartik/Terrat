22aeea46659f353357a5aedecfeae52e62de98b8
tKartik <tyagik2097@gmail.com>
2018-05-21 22:46:27 +0530

    added year slider

--- sketch.js
+++ sketch.js
@@ -10,6 +10,7 @@ var n = 0;
 var buttons;
 var type;
 var slider;
+var slider2;
 
 function meX(lon) {
   lon = radians(lon);
@@ -32,16 +33,15 @@ function preload() {
   table = loadTable('data/india.csv', 'csv', 'header');
 }
 
-function ass() {
-  year = this.value();                                          //assign year on button click
-}
+// function ass() {
+//   year = this.value(); //assign year on button click
+// }
 
 function keyTyped() {
   if (key == 'a') {
-    if(type == 0) {
-        type = 1;                                               //function to change color
-    }
-    else {
+    if (type == 0) {
+      type = 1; //function to change color
+    } else {
       type = 0;
     }
   }
@@ -52,13 +52,17 @@ function setup() {
   canvas.parent('sketch-holder');
   row = table.getRowCount();
 
-  buttons = selectAll('button');
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].mousePressed(ass);
-  }
+  // buttons = selectAll('button');
+  // for (var i = 0; i < buttons.length; i++) {
+  //   buttons[i].mousePressed(ass);
+  // }
 
-  slider = createSlider(2,30,5)
+  slider = createSlider(2, 30, 5)
   slider.parent('s1');
+  slider2 = createSlider(1979, 2016, 1988)
+  slider2.parent('s2')
+
+  // year=int(slider2.value());
 }
 
 
@@ -68,14 +72,14 @@ function draw() {
   translate(350, 350);
   background(255);
   imageMode(CENTER);
-
+  year = int(slider2.value());
 
   if (type == 0) {
     background(255);
   } else {
     image(mapimg, 0, 0);
   }
-  var n=0;
+  var n = 0;
   for (var r = 0; r < row; r++) {
 
     var chk = table.getString(r, 0);
@@ -87,28 +91,28 @@ function draw() {
       loc = table.getString(r, 4);
       typ = table.getString(r, 11);
       var kill = table.getString(r, 19);
-      var hkill =table.getString(r, 16);
+      var hkill = table.getString(r, 16);
       var x = meX(lon) - meX(cX);
       var y = meY(lat) - meY(cY);
 
       noStroke();
-      if(kill>0 || hkill>0){
+      if (kill > 0 || hkill > 0) {
         fill(10, 180, 200, 100);
-      }                                                                 //color for killed
+      } //color for killed
       else {
         fill(160, 50, 20, 100);
       }
 
-      ellipse(x, y, slider.value(), slider.value());                    //drawing the location
+      ellipse(x, y, slider.value(), slider.value()); //drawing the location
 
-      if (dist(mouseX-350, mouseY-350, x, y)<3) {
-        fill(100,200);
-        rect(mouseX-344,mouseY-352,typ.length*7,28);
-        fill(255);                                                      //name of the place
+      if (dist(mouseX - 350, mouseY - 350, x, y) < 3) {
+        fill(100, 200);
+        rect(mouseX - 344, mouseY - 352, typ.length * 7, 28);
+        fill(255); //name of the place
         textSize(11);
-        text(loc,mouseX-340,mouseY-340);
+        text(loc, mouseX - 340, mouseY - 340);
         textSize(11);
-        text(typ,mouseX-340,mouseY-328);
+        text(typ, mouseX - 340, mouseY - 328);
       }
 
       n++
@@ -116,20 +120,25 @@ function draw() {
   }
 
   noStroke();
-   fill(255);
-   rect(150,210,100,180);
-   fill(10, 180, 200, 100);
-   ellipse(117,175,10,10);
-   fill(160, 50, 20, 100);
-   ellipse(117,205,10,10);
+  fill(255);
+  rect(150, 210, 100, 180);
+  fill(10, 180, 200, 100);
+  ellipse(117, 175, 10, 10);
+  fill(160, 50, 20, 100);
+  ellipse(117, 205, 10, 10);
   fill(120);
   textSize(13);
-  text("Attacks with no fatalities",132,180);
-  text("Attacks with fatalities",132,210);
-  text("                 INTERACTIVITY",110,260);
-  text("A - Press A to toggle map on/off",110,290);
-  text("Use the slider below to change the radius of location",110,310);
+  text("Attacks with no fatalities", 132, 180);
+  text("Attacks with fatalities", 132, 210);
+  text("                 INTERACTIVITY", 110, 260);
+  text("A - Press A to toggle map on/off", 110, 290);
+  text("Use the sliders below to change the ", 110, 310);
+  text("year and radius of location", 110, 330);
   textSize(14);
-  text("total number of attacks "+n,110,-290);
+  text("total number of attacks " + n, 110, -290);
+  textSize(25);
+  text(year,140,-260);
+
+
 
 }
